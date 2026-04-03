00f5f2b3677476b24fb765c21e45b50a9b3c4844
MatheusMarquesEiras <matheusmarqueseiras@gmail.com>
2026-04-03 17:28:50 -0300

    fix names of main files

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI
+from app.ui.routes import auth_blueprint, pages_blueprint, services_blueprint
+from app.infra.actions import init_data_base
+from flask import Flask
+import secrets
+import os
 
-app = FastAPI()
+app = Flask(__name__, template_folder='./app/ui/templates', static_folder='./app/ui/static')
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}   
+app.register_blueprint(auth_blueprint)
+app.register_blueprint(pages_blueprint)
+app.register_blueprint(services_blueprint)
+
+app.config['SECRET_KEY'] = secrets.token_hex(16)
+app.config['UPLOAD_FOLDER'] = './uploads/'
+
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+init_data_base()
 
 if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host='localhost', port=3333)
+    app.run(debug=True, host='0.0.0.0',port=3333)
